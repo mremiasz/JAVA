90cbd6de684ade5eed07dec9e0baddb6dec8d76a
mremiasz <39498299+mremiasz@users.noreply.github.com>
2018-08-13 12:48:39 +0200

    dodany wyjątek przy wprowadzaniu danych od urzytk

--- 00_aplikacja_biblioteka/src/utils/DataReader.java
+++ 00_aplikacja_biblioteka/src/utils/DataReader.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import data.Book;
 import data.Magazine;
@@ -16,8 +17,14 @@ public class DataReader {
     }
 
     public int getInt() {
-        int number = sc.nextInt();
-        sc.nextLine();
+        int number = 0;
+        try{
+            number = sc.nextInt();
+        }catch (InputMismatchException e){
+            throw new NumberFormatException("Liczba wprowadzaona w niepoprawnym formacie");
+        } finally {
+            sc.nextLine();
+        }
         return number;
     }
 
@@ -31,12 +38,18 @@ public class DataReader {
         System.out.println("ISBN: ");
         String isbn = sc.nextLine();
         System.out.println("Rok wydania: ");
-        int releaseDate = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Ilość stron: ");
-        int pages = sc.nextInt();
-        sc.nextLine();
-
+        int releaseDate = 0;
+        int pages = 0;
+        try {
+            releaseDate = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Ilość stron: ");
+            pages = sc.nextInt();
+            sc.nextLine();
+        }catch (InputMismatchException e){
+            sc.nextLine();
+            throw e;
+        }
         return new Book(title, author, releaseDate, pages, publisher, isbn);
     }
 
@@ -48,14 +61,22 @@ public class DataReader {
         System.out.println("Wydawca: ");
         String publisher = sc.nextLine();
         System.out.println("Rok wydania: ");
-        int year = sc.nextInt();
-        System.out.println("Miesiąc wydania: ");
-        int month = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Dzień wydania: ");
-        int day = sc.nextInt();
-        sc.nextLine();
-
+        int year = 0;
+        int month = 0;
+        int day = 0;
+        try {
+            year = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Miesiąc: ");
+            month = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Dzień: ");
+            day = sc.nextInt();
+            sc.nextLine();
+        } catch (InputMismatchException e) {
+            sc.nextLine();
+            throw e;
+        }
         return new Magazine(title, publisher, language, year, month, day);
     }
 }
