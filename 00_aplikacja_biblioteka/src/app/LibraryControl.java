6801f52b48a134e7a5bf6531b38e516b6b42ba19
mremiasz <39498299+mremiasz@users.noreply.github.com>
2018-08-13 13:26:45 +0200

    dodanie klasy wewnętrznej
    przeniesiono klasę option i stworzono z niej klasę wewnętrzna

--- 00_aplikacja_biblioteka/src/app/LibraryControl.java
+++ 00_aplikacja_biblioteka/src/app/LibraryControl.java
@@ -80,4 +80,37 @@ public class LibraryControl {
     private void printMagazines() {
         LibraryUtils.printMagazines(library);
     }
+
+    private enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie książki"),
+        ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+        PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie dostępnych magazynów/gazet");
+
+        private int value;
+        private String description;
+
+
+        Option(int value, String desc) {
+            this.value = value;
+            this.description = desc;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        public static Option createFromInt(int option) throws NoSuchElementException {
+            Option result = null;
+
+            try {
+                result = Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e){
+                throw new NoSuchElementException("Brak elementu o wskazanym ID.");
+            }
+            return result;
+        }
+    }
 }
