903a85231e3a23d9f253e533faf44f157ebed36c
mremiasz <39498299+mremiasz@users.noreply.github.com>
2018-08-14 13:55:27 +0200

    zarządzanie odczytem i zapisem biblioteki do pliku

---
+++ 00_aplikacja_biblioteka/src/utils/FileManager.java
@@ -0,0 +1,45 @@
+package utils;
+
+import data.Library;
+
+import java.io.*;
+
+public class FileManager {
+    public static final String FILE_NAME = "Library.out";
+
+    public void writeLibraryToFile(Library lib){
+        try (
+                FileOutputStream fos = new FileOutputStream(FILE_NAME);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                ) {
+            oos.writeObject(lib);
+        } catch (FileNotFoundException e) {
+            System.err.println("Nie odnaleziono pliku "+FILE_NAME);
+        } catch (IOException e) {
+            System.err.println("Bład podczas zapisu danych do pliku "+FILE_NAME);
+        }
+    }
+
+
+    public Library readLibraryFromFile() throws IOException, ClassNotFoundException {
+        Library library = null;
+
+        try(
+                FileInputStream fis = new FileInputStream(FILE_NAME);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                ) {
+            library = (Library)ois.readObject();
+        }catch (FileNotFoundException e){
+            System.err.println("Nie odnaleziono pliku " + FILE_NAME);
+            throw e;
+        } catch (IOException e ){
+            System.err.println("Bład podczas odczytu danych z pliku " + FILE_NAME);
+            throw e;
+        }catch (ClassNotFoundException e) {
+            System.err.println("Nieprawidłowy format pliku.");
+            throw e;
+        }
+
+        return library;
+    }
+}
