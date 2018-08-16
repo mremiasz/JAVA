8bee039d4f7928428a5571911ae6b0cbabb6d8b0
mremiasz <michalremiasz@gmail.com>
2018-08-16 11:26:47 +0200

    dodanie uzytkownika

---
+++ 00_aplikacja_biblioteka/src/data/User.java
@@ -0,0 +1,67 @@
+package data;
+
+import java.io.Serializable;
+
+public abstract class User implements Serializable{
+    private static final long serialVersionUID = 14651345643541684L;
+
+    private String firstName;
+    private String lastName;
+    private String pesel;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public User (String firstName, String lastName, String pesel){
+        super();
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.pesel=pesel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        return pesel != null ? pesel.equals(user.pesel) : user.pesel == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (pesel != null ? pesel.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " "+ lastName + " " + pesel;
+    }
+}
